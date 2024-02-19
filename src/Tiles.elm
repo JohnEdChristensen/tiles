63f524dafb1b762f64460b88542e03d728eff482
John Christensen <johnedchristensen@gmail.com>
2024-02-19 10:06:57 -0600

    fixup! Removed template code. Cleaned up SVG->Geometry

--- src/Tiles.elm
+++ src/Tiles.elm
@@ -1,7 +1,4 @@
--- Press a button to generate a random number between 1 and 6.
---
--- Read how it works:
---   https://guide.elm-lang.org/effects/random.html
+-- play with tiles
 --
 
 
@@ -12,8 +9,6 @@ import Color as SvgC
 import Element exposing (..)
 import Element.Background as Background
 import Element.Events exposing (..)
-import Random
-import Time
 import TypedSvg exposing (polygon, svg)
 import TypedSvg.Attributes as SvgA
 import TypedSvg.Core exposing (Svg)
@@ -40,62 +35,16 @@ main =
 -- MODEL
 
 
-type Status
-    = IsRolling
-    | Rolled
-
-
 type alias Model =
-    { dieFace : Int
-    , state : Status
+    { x : Int
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model 1 Rolled
+    ( Model 1
     , Cmd.none
     )
-
-
-
--- UPDATE
-
-
-type Msg
-    = StartRoll
-    | EndRoll
-    | NewFace Int
-    | Tick Time.Posix
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        Tick _ ->
-            case model.state of
-                IsRolling ->
-                    ( Model (modBy 6 (model.dieFace + 1)) IsRolling
-                    , Cmd.none
-                    )
-
-                _ ->
-                    ( model, Cmd.none )
-
-        StartRoll ->
-            ( Model -1 IsRolling
-            , Cmd.none
-            )
-
-        EndRoll ->
-            ( model
-            , Random.generate NewFace (Random.int 1 6)
-            )
-
-        NewFace newFace ->
-            ( Model newFace Rolled
-            , Cmd.none
-            )
 
 
 
@@ -104,7 +53,22 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    Time.every 50 Tick
+    Sub.none
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Default
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Default ->
+            ( model, Cmd.none )
 
 
 
@@ -179,7 +143,7 @@ tilesSvg =
 
 -- SVG Configuration
 --
--- The parent SVG takes up the whole screen, and uses viewBox 
+-- The parent SVG takes up the whole screen, and uses viewBox
 -- to specify the coordiantes
 -- https://ellie-app.com/cp8m289xjgQa1
 
@@ -227,52 +191,63 @@ tileClipPaths =
             [ parallelogramSvg 60 0 10.0 ( 0, 0 ) SvgC.lightRed ]
         ]
 
+-- Geometry
 
 type alias Radians =
     Float
+
+
+type alias Degrees =
+    Float
+
+
+degreesToRadians : Degrees -> Radians
+degreesToRadians degrees =
+    degrees * (pi / 180)
 
 
 type alias Point =
     ( Float, Float )
 
 
+addPoints : Point -> Point -> Point
+addPoints ( x1, y1 ) ( x2, y2 ) =
+    ( x1 + x2, y1 + y2 )
+
+
 type alias Quad =
     List Point
-
-
-degreesToRadians : Float -> Radians
-degreesToRadians degrees =
-    degrees * (pi / 180)
-
-
-
--- Function to calculate the vertices of the parallelogram
 
 
 parallelogramVerts : Radians -> Float -> Quad
 parallelogramVerts angle length =
     let
-        -- Origin
+        --    c_______d
+        --    /      /
+        --   /      /
+        --  /______/
+        -- a       b
         a =
             ( 0, 0 )
 
-        -- l1 units along the x-axis
         b =
             ( length, 0 )
 
-        -- l2 units at angle a from A
         c =
             ( length * cos angle, length * sin angle )
 
-        -- Adding vector AC to point B to get D
+        -- Adding vector AC to AB to get D
         d =
-            ( Tuple.first b + Tuple.first c, Tuple.second b + Tuple.second c )
+            addPoints b c
     in
     [ a, b, d, c ]
 
 
+-- Geometry -> SVG
 
--- Function to create an SVG element for the parallelogram
+clipPath : Degrees -> String
+clipPath angleDegrees =
+    "myClipPath" ++ String.fromFloat angleDegrees
 
 
 parallelogramSvg : Float -> Float -> Float -> Point -> SvgC.Color -> Svg msg
@@ -284,16 +259,17 @@ parallelogramSvg angleDegrees shapeRotationDegrees length ( coordX, coordY ) col
         -- Calculate vertices
         quad =
             parallelogramVerts angleRadians length
-
-        clipPath =
-            "myClipPath" ++ String.fromFloat angleDegrees
     in
     polygon
         [ SvgA.points quad
         , SvgA.fill (SvgT.Paint color)
         , SvgA.stroke <| SvgT.Paint SvgC.black
         , SvgA.strokeWidth <| SvgT.pt 0.2
-        , SvgA.style <| "paint-order:fill;" ++ " clip-path: url(#" ++ clipPath ++ ")"
+        , SvgA.style <|
+            "paint-order:fill;"
+                ++ " clip-path: url(#"
+                ++ clipPath angleDegrees
+                ++ ")"
         , SvgA.transform
             [ SvgT.Translate coordX coordY
             , SvgT.Rotate shapeRotationDegrees 0 0
